2f05ac2e464fee34e7e2eac825d4c44448b9db0a
letel005 <letel005@umn.edu>
2023-03-23 14:03:35 -0500

    initial example for styled components

---
+++ src/examples/example4-styled-components/Example4.js
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled'
+
+export const Example4 = () => {
+    return(<><button>my button</button></>)
+}
---
+++ src/examples/example4-styled-components/Example4Complete.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+const MyButton = styled.button`
+    background-color: hotpink;
+`
+
+export const Example4Complete = () => {
+    return(<><MyButton>my button 2</MyButton></>)
+}
