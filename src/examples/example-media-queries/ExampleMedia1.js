e1662cd44913c6767fe568254f89da8206afd470
melonSkin76 <guo00370@umn.edu>
2023-03-28 19:55:47 -0500

    Some Improvements for Better Demo
    1. Moved the completed part into the comments of basic example, so the audience can tell I am changing and operating on the same file
    2. Switched the demo css property from text color to background color, making the media queries feature more visible for audience

--- src/examples/example-media-queries/ExampleMedia1.js
+++ src/examples/example-media-queries/ExampleMedia1.js
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+
+// ========== Media Queries in a Separate CSS File ==========
 import './ExampleMedia1.css'
 
 export function ExampleMedia1() {
@@ -5,3 +8,51 @@ export function ExampleMedia1() {
     Some text.
   </div>
 }
+
+
+
+// ========== Media Queries in Javascript with Breakpoints Values Stored in a const Variable ==========
+// export const breakpoints = [600, 800]
+
+// const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`)
+
+// export function ExampleMedia1() {
+//   return <div
+//     css={[
+//       {
+//         backgroundColor: 'green',
+//         [mq[0]]: {
+//           backgroundColor: 'gray'
+//         },
+//         [mq[1]]: {
+//           backgroundColor: 'hotpink'
+//         }
+//       },
+//       {fontSize: 'xxx-large'}
+//     ]}
+//   >
+//     Some text.
+//   </div>
+// }
+
+
+
+// ========== Define the CSS Property (color) at Each Media Query (breakpoint) Using an Array ==========
+// import facepaint from 'facepaint'
+
+// export const breakpoints = [576, 768]
+
+// const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
+
+// export function ExampleMedia1() {
+//   return <div
+//     css={[
+//       mq({
+//         backgroundColor: ['green', 'gray', 'hotpink']
+//       }),
+//       {fontSize: 'xxx-large'}
+//     ]}
+//   >
+//     Some text.
+//   </div>
+// }
